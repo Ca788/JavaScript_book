1c59bfa1a83a9b263b8d6f894a5c9efee1214dce
Carlos Lima <duca_liima@icloud.com>
2023-09-08 20:03:43 -0300

    Hoje estudei os conceitos de manipulação de strings nos exemplos estudados foram aplicados os métodos CharAt(), toLowerCase(), toUpperCase(), trim(), substr(). Dentre os exemplos foi criado um programa para um evento (ficticio), que lê o nome completo do participante e gera sua credencial.

---
+++ cap07/js/ex7_4.js
@@ -0,0 +1,23 @@
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nome = frm.inNome.value.trim();
+
+  if (!nome.includes(" ")) {
+    alert("Informe o nome completo...");
+    return;
+  }
+  const priEspaco = nome.indexOf(" ");
+  const ultEspaco = nome.lastIndexOf(" ");
+  //copia nome e sobrenome usando os parâmetros do substr()
+  const cracha = nome.substr(0, priEspaco) + nome.substr(ultEspaco);
+
+  resp.innerText = `Crácha: ${cracha}`;
+});
+
+//Com o uso do método indexOf(), indentificamos a posição do primeiro espaço do nome do participante.
+//Ja o lasIndexOf() é utilizado para obter a posição do último espaço. Em seguida a variável cracha recebe duas substrings.
+//A primeira da posição inicial do nome, com a quantidade de caracteres e a segunda com a posição do último espaço.
