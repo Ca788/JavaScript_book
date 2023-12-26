2ac0fe9c05ce78d17d556c573e2d4776f020f79f
Carlos Lima <duca_liima@icloud.com>
2023-12-26 11:00:22 -0300

    Criei um exemplo para testar o uso do console.table, que é uma forma mais organizada para acessar objetos no console.

---
+++ ScriptsAleatorios/console.table.js
@@ -0,0 +1,10 @@
+const userData = {
+  name: "Carlos Lima",
+};
+
+userData.age = 25;
+userData.email = "Carloslima@email.com";
+userData["IsMarried"] = false;
+
+console.table(userData);
+console.log(userData);
